949a8e8b41c0b9ed4aabd74b5704315b1b86d91f
Andrei Koliada <detroyent@gmail.com>
2022-08-21 21:40:45 +0300

    add homework20

--- homework20/app.js
+++ homework20/app.js
@@ -7,7 +7,7 @@ const productsArr  =  getProductList();
 server.set('view engine', 'ejs');
 server.set('views', './views');
 
-server.use(express.static('./public'))
+server.use(express.static(__dirname + '/public'))
 
 server.get('/', (req, res) => {
     res.render('index.ejs')
@@ -15,6 +15,12 @@ server.get('/', (req, res) => {
 
 server.get('/products', (req, res) => {
     res.send(JSON.stringify(productsArr));
+})
+
+server.get('/product/:id', (req, res) => {
+    let { id } = req.params;
+    let product =  productsArr[id];
+    res.render('product', {product})
 })
 
 server.listen(4308);
--- homework20/public/index.js
+++ homework20/public/index.js
@@ -17,26 +17,44 @@ button.addEventListener('click', async () => {
     mainPage.appendChild(productList);
 
     data.forEach( (item, index) => {
+        let link = document.createElement('a')
+        link.id = index;
+        link.href = `/product/${index}`;
+        productList.appendChild(link);
         let product = document.createElement('div');
         product.classList.add('product');
         product.id = index;
-        productList.appendChild(product)
+        link.appendChild(product)
         let img = document.createElement('img');
-        img.src = item.img;
+        img.src = item.img;        
+        img.id = index;
         product.appendChild(img);
-        let name = document.createElement('h1');
+        let name = document.createElement('h1');       
+        name.id = index;
         name.innerHTML = item.name;
         product.appendChild(name);        
-        let category = document.createElement('p');
+        let category = document.createElement('p');       
+        category.id = index;
+        category.classList.add('category');
         category.innerHTML = item.category;
         product.appendChild(category);
-        let price = document.createElement('h2');
-        price.innerHTML = item.category;
+        let price = document.createElement('h2');       
+        price.id = index;
+        price.innerHTML = item.price;
         product.appendChild(price);
+    })
 
-        loaderPage.className = 'loader-page hidden';
-        setTimeout(() => {
-            loaderPage.remove();
-        }, 500)
+    loaderPage.className = 'loader-page hidden';
+    setTimeout(() => {
+        loaderPage.remove();
+    }, 500)
+
+    productList.addEventListener('click', (ev) => {
+        let productId = +ev.target.id;
+        console.log(productId);
+        async function sendId(id) {
+            await axios.get(`/product/${id}`)
+        }
+        sendId(productId);
     })
 })
